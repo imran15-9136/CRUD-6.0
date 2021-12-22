64189c963933808d33523b4529c7a96cc8e594fa
Imran Khan <imran_bof@live.com>
2021-12-22 16:33:40 +0600

    item

---
+++ Models/Request/ItemCreateDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Request
+{
+    public class ItemCreateDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double Price { get; set; }
+        public double Vat { get; set; }
+        public int ItemCategoryId { get; set; }
+        public DateTime? Created { get; set; }
+    }
+}
