994fb8c122318ac5f2b44575ea700dc41b2d2825
Imran Khan <imran_bof@live.com>
2021-12-23 17:39:46 +0600

    repository

--- CRUD.Repository.Abstraction/Base/IRepository.cs
+++ CRUD.Repository.Abstraction/Base/IRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,20 @@ namespace CRUD.Repository.Abstraction.Base
 {
     public interface IRepository<T> where T : class
     {
+        Task<bool> AddAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> RemoveAsync(T entity);
+        Task<ICollection<T>> GetAllAsync();
+        Task<T> GetFirstorDefault(Expression<Func<T, bool>> predicate);
+        Task<T> GetById(int id);
+        IQueryable<T> Get(Expression<Func<T, bool>> predicate);
+        bool UpdateRange(IList<T> entity);
+        Task<bool> UpdateRangeAsync(IList<T> entity);
+        bool RemoveRange(IList<T> entity);
+        Task<bool> RemoveRangeAsync(IList<T> entity);
+        Task<bool> AddRangeAsync<T>(ICollection<T> entities) where T : class;
+        IQueryable<T> GetAsNoTracking(Expression<Func<T, bool>> predicate);
+        T GetFirstOrDefaultAsNoTracking(Expression<Func<T, bool>> predicate);
+        Task<T> GetFirstOrDefaultAsNoTrackingAsync(Expression<Func<T, bool>> predicate);
     }
 }
--- CRUD.Repository/Base/Repository.cs
+++ CRUD.Repository/Base/Repository.cs
@@ -1,13 +1,102 @@
 ﻿using CRUD.Repository.Abstraction.Base;
+using Database.Database;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CRUD.Repository.Base
 {
-    public class Repository<T> : IRepository<T> where T : class
+    public abstract class Repository<T> : IRepository<T> where T : class
     {
+        ApplicationDbContext _db;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async virtual Task<bool> AddAsync(T entity)
+        {
+            _db.Add(entity);
+            return await _db.SaveChangesAsync() > 0;
+        }
+
+        public async virtual Task<bool> UpdateAsync(T entity)
+        {
+            _db.Entry(entity).State = EntityState.Modified;
+            return await _db.SaveChangesAsync() > 0;
+        }
+
+        public async virtual Task<bool> RemoveAsync(T entity)
+        {
+            _db.Remove(entity);
+            return await _db.SaveChangesAsync() > 0;
+        }
+
+        public async virtual Task<ICollection<T>> GetAllAsync()
+        {
+            return await _db.Set<T>().ToListAsync();
+        }
+
+        public async virtual Task<T> GetFirstorDefault(Expression<Func<T, bool>> predicate)
+        {
+            return await _db.Set<T>().FirstOrDefaultAsync(predicate);
+        }
+
+        public async virtual Task<T> GetById(int id)
+        {
+            return await _db.Set<T>().FindAsync(id);
+        }
+
+        public virtual IQueryable<T> Get(Expression<Func<T, bool>> predicate)
+        {
+            return _db.Set<T>().Where(predicate).AsQueryable();
+        }
+
+        public virtual bool UpdateRange(IList<T> entity)
+        {
+            _db.Set<T>().UpdateRange(entity);
+            return _db.SaveChanges() > 0;
+        }
+        public virtual async Task<bool> UpdateRangeAsync(IList<T> entity)
+        {
+            _db.Set<T>().UpdateRange(entity);
+            return await _db.SaveChangesAsync() > 0;
+        }
+
+        public bool RemoveRange(IList<T> entity)
+        {
+            _db.Set<T>().RemoveRange(entity);
+            return _db.SaveChanges() > 0;
+        }
+        public async Task<bool> RemoveRangeAsync(IList<T> entity)
+        {
+            _db.Set<T>().RemoveRange(entity);
+            return await _db.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> AddRangeAsync<T>(ICollection<T> entities) where T : class
+        {
+            await _db.AddRangeAsync(entities);
+            return await _db.SaveChangesAsync() > 0;
+        }
+
+        public virtual IQueryable<T> GetAsNoTracking(Expression<Func<T, bool>> predicate)
+        {
+            return _db.Set<T>().AsNoTracking().Where(predicate);
+        }
+
+        public virtual T GetFirstOrDefaultAsNoTracking(Expression<Func<T, bool>> predicate)
+        {
+            return _db.Set<T>().AsNoTracking().FirstOrDefault(predicate);
+        }
+
+        public virtual async Task<T> GetFirstOrDefaultAsNoTrackingAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _db.Set<T>().AsNoTracking().FirstOrDefaultAsync(predicate);
+        }
     }
 }
