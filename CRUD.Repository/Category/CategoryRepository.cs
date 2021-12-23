c4ddef0aa3f9b7b58056d8b2148cdc539c074ca4
Imran Khan <imran_bof@live.com>
2021-12-23 17:45:33 +0600

    category_repo

---
+++ CRUD.Repository/Category/CategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using CRUD.Repository.Abstraction.Category;
+using CRUD.Repository.Base;
+using Database.Database;
+using Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.Repository.Category
+{
+    public class CategoryRepository:Repository<ItemCategory>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext context):base(context)
+        {
+            _context = context;
+        }
+    }
+}
