1197f01f1cca2f2f059758dd59d52891ee739848
imran15-9136 <imran_bof@live.com>
2022-01-13 07:26:38 +0600

    sp_itemgat by id

---
+++ Database/Migrations/20220113012246_Create_Procedure_SP_GetItemById.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    public partial class Create_Procedure_SP_GetItemById : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            CREATE PROCEDURE SP_GetItemById
+			@Id int
+			AS 
+
+			IF 1 = 0
+
+					BEGIN
+
+					SET FMTONLY OFF
+
+					END
+
+			SELECT * FROM Items
+			WHERE Id = @Id
+
+			--EXEC SP_GetItemById 1
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"      
+            DROP PROCEDURE SP_GetItemById
+
+            @Id int
+
+            AS
+
+
+            IF 1 = 0
+
+                    BEGIN
+
+                    SET FMTONLY OFF
+
+                    END
+
+            SELECT * FROM Items
+
+            WHERE Id = @Id
+
+            --EXEC SP_GetItemById 1
+            ");
+        }
+    }
+}
