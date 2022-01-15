4bf19456737433f056d02fcd31e02f0ffd2a3e50
imran15-9136 <imran_bof@live.com>
2022-01-15 07:53:00 +0600

    ado.net

--- CRUD.Repository/Items/ItemRepository.cs
+++ CRUD.Repository/Items/ItemRepository.cs
@@ -1,10 +1,13 @@
 ﻿using CRUD.Repository.Abstraction.Items;
 using CRUD.Repository.Base;
+using CRUD.Repository.Conection;
 using Database.Database;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Models.Entity;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,9 +17,37 @@ namespace CRUD.Repository.Items
     public class ItemRepository: Repository<Item>, IItemRepository
     {
         private readonly ApplicationDbContext _dbcontext;
+        string connectinString = ConnectionString.CName;
         public ItemRepository(ApplicationDbContext dbcontext):base(dbcontext)
         {
             _dbcontext = dbcontext;
+        }
+
+        public async override Task<IEnumerable<Item>> GetItemsAsync()
+        {
+            List<Item> items = new List<Item>();
+            using (SqlConnection con = new SqlConnection(connectinString))
+            {
+                SqlCommand cmd = new SqlCommand("SP_GetItems", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Item item = new Item();
+                    item.Id = Convert.ToInt32(rdr["ID"]);
+                    item.Name = rdr["Name"].ToString();
+                    item.Price = Convert.ToInt32(rdr["Price"]);
+                    item.Vat = Convert.ToInt32(rdr["Vat"]);
+                    item.ItemCategoryId = Convert.ToInt32(rdr["ItemCategoryId"]);
+                    item.Created = Convert.ToDateTime(rdr["Created"]);
+                    item.ImagePath = rdr["ImagePath"].ToString();
+                    items.Add(item);
+                }
+                con.Close();
+            }
+            return items;
         }
 
         public async override Task<Item> GetById(int id)
@@ -28,5 +59,7 @@ namespace CRUD.Repository.Items
             }
             return null;
         }
+
+
     }
 }
