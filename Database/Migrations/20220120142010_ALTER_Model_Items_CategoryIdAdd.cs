b37606013c9e2b66f267b54bb43a957d256325f9
imran15-9136 <imran15-9136@diu.edu.bd>
2022-01-20 20:22:41 +0600

    migraiton

---
+++ Database/Migrations/20220120142010_ALTER_Model_Items_CategoryIdAdd.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    public partial class ALTER_Model_Items_CategoryIdAdd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_ItemCategories_ItemCategoryId",
+                table: "Items");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ItemCategoryId",
+                table: "Items",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_ItemCategories_ItemCategoryId",
+                table: "Items",
+                column: "ItemCategoryId",
+                principalTable: "ItemCategories",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_ItemCategories_ItemCategoryId",
+                table: "Items");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ItemCategoryId",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_ItemCategories_ItemCategoryId",
+                table: "Items",
+                column: "ItemCategoryId",
+                principalTable: "ItemCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
